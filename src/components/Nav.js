a79e4c697dee069d2fb4febbbdab36c778b1182e
Erika Kawashima <marshallekm@gmail.com>
2022-07-07 19:53:43 +0900

    nav

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nav(){
+  return (
+    <div>
+      <nav className= "navbar">
+        <h1>Meme Generator</h1>
+        <h2>React Course Project</h2>
+      </nav>
+    </div>
+  )
+}
