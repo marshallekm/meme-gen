f36c2c9f1b97ed11bad5186a1ceda70cdb721e49
Erika Kawashima <marshallekm@gmail.com>
2022-08-25 20:27:23 +0900

    forms work ok

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -23,35 +23,58 @@ getMemeImage(newMemeImage => {
   })
 }
 
+const handleChange = (event) => {
+const {name, value} = event.target
+getMemeImage(prevMemeImage => {
+  return {
+    ...prevMemeImage,
+    [name]:value
+  }
+})
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+}
+
+
   return (
 <div>
   <div className= "form--div">
-    <div className= "input--div">
+    <div className ="input--div">
+     <form onSubmit = {handleSubmit}>
         <input
         type= "text"
         placeholder="Top"
         className="form1"
+        name="topText"
+        onChange = {handleChange}
+        value = {memeImage.topText}
          />
 
         <input
         type="text"
         placeholder="Bottom"
         className="form2"
+        name= "bottomText"
+        onChange ={handleChange}
+        value ={memeImage.bottomText}
         />
-      </div>
-<div className="button-div">
-      <button
-        className="form--button"
-        onClick={getRandomPic}
+      <div className="button-div">
+        <button
+          className="form--button"
+          onClick={getRandomPic}
         >
           Click me!
-      </button>
-</div>
+        </button>
+      </div>
+    </form>
+  </div>
 
     <div className ="meme">
       <img src={memeImage.randomImage} className="meme--image" />
-      <h2 className = "meme--text top">Hey</h2>
-      <h2 className = "meme--text bottom">Hi</h2>
+      <h2 className = "meme--text top">{memeImage.topText}</h2>
+      <h2 className = "meme--text bottom">{memeImage.bottomText}</h2>
     </div>
   </div>
 </div>
